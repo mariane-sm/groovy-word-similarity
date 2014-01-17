f051691971a27a2ae3e20b053bff5cafabb0813e
Mariane Siqueira Machado <mariane.machado@gmail.com>
2014-01-17 15:54:39 -0200

    Using slf4j

--- src/main/groovy/Query.groovy
+++ src/main/groovy/Query.groovy
@@ -2,7 +2,8 @@ package wordSimilarityChecker
 
 import org.springframework.stereotype.Component;
 import javax.inject.Inject;   
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 class Query {
@@ -10,7 +11,10 @@ class Query {
 	@Inject
 	private WordSimilarityChecker checker
 
+	final static Logger logger = LoggerFactory.getLogger(Query.class);
+
 	def checkQueriesSimilarity(String str1, String str2) {
-		println checker.check(str1,str2)
+		println checker.check(str1, str2)
+		logger.info('Finishing log.');
 	}
 }
