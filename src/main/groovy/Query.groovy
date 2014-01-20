4ea8ce9fbb0bf4fa1bb88488e6eacef8dbe1aba3
Mariane Siqueira Machado <mariane.machado@gmail.com>
2014-01-20 13:48:49 -0200

    Using annotations for logging

--- src/main/groovy/Query.groovy
+++ src/main/groovy/Query.groovy
@@ -2,19 +2,17 @@ package wordSimilarityChecker
 
 import org.springframework.stereotype.Component;
 import javax.inject.Inject;   
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import groovy.util.logging.Slf4j
 
-@Component
+@Slf4j
+@Component 
 class Query {
 	
 	@Inject
 	private WordSimilarityChecker checker
 
-	final static Logger logger = LoggerFactory.getLogger(Query.class);
-
 	def checkQueriesSimilarity(String str1, String str2) {
 		println checker.check(str1, str2)
-		logger.info('Finishing log.');
+		log.info('Finishing log.');
 	}
 }
